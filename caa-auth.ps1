b10d40fc9a877fd9199432bb04bc0bd4e3bceaa7
Ryan Payne <ryanj.payne@outlook.com>
2021-09-09 17:18:34 -0400

    Create caa-auth.ps1

---
+++ caa-auth.ps1
@@ -0,0 +1,44 @@
+function Get-Credentials {
+
+    Param ()
+
+    # Set Vars
+    $jumpAccount = ""
+    $region = ""
+    $userName = ""
+    $profile = ""
+    $roleName = ""
+    $accounts =@(
+        ""
+        ""
+    )
+
+    # Authenticate with Stored Profile and MFA
+    Set-AWSCredentials -ProfileName $profile
+    Set-DefaultAWSRegion -Region $region
+    $mfaArn = "arn:aws-us-gov:iam::" + $jumpAccount + ":mfa/" + $userName
+    $mfaToken = Read-Host "Please enter your MFA Token"
+    $tempCredentials = Get-STSSessionToken -DurationInSeconds 3600 -ProfileName $profile -SerialNumber $mfaArn -TokenCode $mfaToken
+
+    # Build Credential and Invoke Next-Function Per Account
+    foreach ($i in $accounts){
+
+        $roleArn = "arn:aws-us-gov:iam::"+ $i + ":role/" + $roleName   
+        $credential = (Use-STSRole -RoleArn $roleArn -DurationInSeconds 3600 -RoleSessionName "CAA" -Credential $tempCredentials).Credentials
+    
+        Next-Function -credential $credential
+    }
+}
+
+function Next-Function {
+
+    Param (
+        $credential
+    )
+
+    Set-AWSCredentials -AccessKey $credential.AccessKeyId -SecretKey $credential.SecretAccessKey -SessionToken $credential.SessionToken
+    
+    <# Insert Code #>
+}
+
+Get-Credentials
